2490fe66f2094f0815c693c8469651917f86c3af
Rajat Sharma <rajat.sharma@quovantis.com>
2020-06-23 23:04:54 +0530

    Jira api token authentication added

--- app.js
+++ app.js
@@ -1,11 +1,26 @@
 console.log("Om Ganeshay Namah!");
 const http = require("http");
 const express = require("express");
+const JiraClient = require("jira-connector");
 
 var app = express();
+
+const jira = new JiraClient({
+  host: "startcpw.atlassian.net",
+  strictSSL: true, // One of optional parameters
+  basic_auth: {
+    email: "rajat.sharma@quovantis.com",
+    api_token: "bHNZ5tbMKbHvSSMfblVvD9C0",
+  },
+});
 
 http.createServer(app).listen(3000);
 
 app.get("/home", (req, res) => {
-  res.end("Om Ganeshay Namah!");
+  res.send("Om Ganeshay Namah!");
+});
+
+app.get("/issues", async (req, res) => {
+  const issue = await jira.issue.getIssue({ issueKey: "S4-6776" });
+  res.json(issue);
 });
