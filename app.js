2ca57842b871df464c0febd3293267aa01e0b280
Rajat Sharma <rajat.sharma@quovantis.com>
2020-06-23 22:30:25 +0530

    Express js added

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
 console.log("Om Ganeshay Namah!");
+const http = require("http");
+const express = require("express");
+
+var app = express();
+
+http.createServer(app).listen(3000);
+
+app.get("/home", (req, res) => {
+  res.end("Om Ganeshay Namah!");
+});
