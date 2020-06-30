f698188c25a40f6f5340201c73197b9b5e73c3af
Rajat Sharma <rajat.sharma@quovantis.com>
2020-06-30 21:27:29 +0530

    Jira tokens commented

--- jira_auth/jiraAuth.js
+++ jira_auth/jiraAuth.js
@@ -1,11 +1,11 @@
 const JiraClient = require("jira-connector");
-const JiraCredentials = require("../jiraCredentials.json");
+// const JiraCredentials = require("../jiraCredentials.json");
 const authenticate = new JiraClient({
   host: "startcpw.atlassian.net",
-  strictSSL: true, // One of optional parameters
+  strictSSL: true,
   basic_auth: {
-    email: process.env.EMAIL || JiraCredentials.email,
-    api_token: process.env.API_TOKEN || JiraCredentials.api_token,
+    email: process.env.EMAIL || ``, // JiraCredentials.email,
+    api_token: process.env.API_TOKEN || ``, // JiraCredentials.api_token,
   },
 });
 
