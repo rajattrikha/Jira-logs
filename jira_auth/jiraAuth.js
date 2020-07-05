9ba7142220c57796e8c65d5ba5e825186168e6a4
Rajat Sharma <rajat.sharma@quovantis.com>
2020-07-05 16:18:19 +0530

    Process env used for auth

--- jira_auth/jiraAuth.js
+++ jira_auth/jiraAuth.js
@@ -1,11 +1,11 @@
-const JiraClient = require("jira-connector");
-const JiraCredentials = require("../jiraCredentials.json");
+const JiraClient = require('jira-connector');
+const JiraCredentials = require('../jiraCredentials.json');
 const authenticate = new JiraClient({
-  host: "box007.atlassian.net",
+  host: 'box007.atlassian.net',
   strictSSL: true,
   basic_auth: {
-    email: JiraCredentials.email,
-    api_token: JiraCredentials.api_token,
+    email: process.env.EMAIL || JiraCredentials.email,
+    api_token: process.env.API_TOKEN || JiraCredentials.api_token,
   },
 });
 
